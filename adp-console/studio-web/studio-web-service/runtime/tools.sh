8d53c2da6ce931937003b2637d522776849275e3
qbc-aishu <bincheng.qian@aishu.cn>
2025-12-25 14:55:41 +0800

    delete useless resources

--- adp-console/studio-web/studio-web-service/runtime/tools.sh
+++
@@ -1,7 +0,0 @@
-export registry="acr.aishu.cn"
-export repository="public/studio-web-service-builder-node"
-export nodeversion="18.7.0"
-export amd64="x64"
-export arm64="arm64"
-export timedate=$(date "+%Y%m%d")
-export imagename="$registry/$repository:$nodeversion"
